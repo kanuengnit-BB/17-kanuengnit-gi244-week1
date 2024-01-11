14e52d590f458bf5ab8c16467c5f1124c8c99cc6
kanuengnit-BB <151009870+kanuengnit-BB@users.noreply.github.com>
2024-01-11 10:53:29 +0700

    edit  inventory script
    +DisplayInventory
    +inventoryList
    +remove/add item

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
+    //Array
+    private string[] intventoryArray = new []{"Sword","Shield","Potion"};
+    
+    //how to move element array => list
+    private List<string> inventoryList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventoryList = new List<string>(intventoryArray);
+        DisplayInventory();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            inventoryList.Add("Helmet");
+            DisplayInventory();
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            inventoryList.Remove("Sword");
+            DisplayInventory();
+        }
     }
+
+    void DisplayInventory()
+    {
+        int i=0;
+        Debug.Log("Inventory:");
+        foreach (var item in inventoryList)
+        {
+            Debug.Log($"{++i}. {item}");
+            
+        }
+    }
+    
+    
 }
+
